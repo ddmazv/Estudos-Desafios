a2ea3e2b996bb5540e269e5827e04239b0a3320e
ddazv <dd.mazv@gmail.com>
2024-10-29 18:42:02 -0300

    atualização exercicios

--- Estudos/DIO/Java Basico/CollectionsJava/JavaCollections/src/collections/java/Main.java
+++ Estudos/DIO/Java Basico/CollectionsJava/JavaCollections/src/collections/java/Main.java
@@ -7,7 +7,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		System.out.println("testando Lista de tarefas");
+
+		ListaTarefas lt = new ListaTarefas(); 
+		
+		lt.adicionarTarefa("fazer codigo");
+		lt.adicionarTarefa("fazer cafe");
+		lt.adicionarTarefa("dormir");
+		lt.adicionarTarefa("continue;");
+		
+		System.out.println(lt.obterNumeroTotalTarefas());
+		lt.obterDescricoesTarefas();
+		System.out.println();
+		
+		lt.removerTarefa("fazer codigo");
+		System.out.println(lt.obterNumeroTotalTarefas());
+		lt.obterDescricoesTarefas();
+	System.out.println();
+	System.out.println("testando carrinho de compras");
+	System.out.println();
 		CarrinhoCompras cc = new CarrinhoCompras();
 		
 		cc.adicionarItem("Celular", 5000.00, 1);
@@ -18,6 +36,8 @@ public class Main {
 		cc.removerItem("Pc Gamer");
 		cc.exibirItens();
 		System.out.println(cc.calcularValorTotal());
+
+		
 		
 	}
 
