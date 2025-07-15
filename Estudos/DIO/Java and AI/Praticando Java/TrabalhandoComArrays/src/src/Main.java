2f7a36c337e7223bcfad8b1b5ac22d6d65d53907
ddazv <dd.mazv@gmail.com>
2025-07-15 16:06:16 -0300

    atualizando tailwindcss

---
+++ Estudos/DIO/Java and AI/Praticando Java/TrabalhandoComArrays/src/src/Main.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> test = new ArrayList();
+		
+		
+		test.add(1);
+		
+		test.add(0);
+		
+		System.out.println(test.get(0));
+	}
+
+}
